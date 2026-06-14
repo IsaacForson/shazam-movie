8875c80027ec04b91513b6594da334cf6128d208
Youngtech303 <youngtech303@gmail.com>
2026-06-14 22:18:59 +0000

    Add suppressHydrationWarning to RootLayout for improved hydration handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${fraunces.variable} ${inter.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
